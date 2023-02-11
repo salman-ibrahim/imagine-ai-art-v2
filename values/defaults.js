c82e34dda253d6f258f9d7df4d8df59a83cf9dcc
salman-ibrahim <s2018065001@umt.edu.pk>
2023-02-11 17:42:42 +0500

    iap sdk added

--- values/defaults.js
+++ values/defaults.js
@@ -3,6 +3,8 @@ export const defaults = {
     artCost: 2,
 
     // Ads
+    publisherId: 'pub-2313151984818754',
     rewsardedAdUnitId: 'ca-app-pub-2313151984818754/5805464636',
     bannerAdUnitId: 'ca-app-pub-2313151984818754/4056999199',
+
 }
